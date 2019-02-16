8c0a770848ee40fef3b296ca382cf7d0a164a786
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-02-16 20:48:51 +0100

    Different keyboard press

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
 from Environment import Environment
 import keyboard
+import sys, tty, termios
+
+
+def get_character():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 # environment without obstacles
 # env = Environment()
@@ -10,7 +22,10 @@ env = Environment(100, 80, [[0, 60, 20, 23],
 
 while True:  # making a loop
     env.render()
-    if keyboard.is_pressed('q'):  # if key 'q' is pressed
+
+    char = get_character()
+
+    if char == "q":  # if key 'q' is pressed
         print('Closing environment')
         break  # finishing the loop
 
