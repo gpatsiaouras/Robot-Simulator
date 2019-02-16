9b4eecf00179c35a8e8c9080c8df4fea0fe69a54
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-02-16 20:00:45 +0100

    add obstacles in the environment

--- src/Environment.py
+++ src/Environment.py
@@ -5,18 +5,36 @@ import matplotlib.pyplot as plt
 
 class Environment:
 
-    def __init__(self, width=100, height=100, robot=None):
+    def __init__(self, width=100, height=100, obstacles=None, robot=None):
+        """
+        :param width:
+        :param height:
+        :param obstacles:       list of lists where each sublist is [x_from, x_to, y_from, y_to]
+        :param robot:           list of robots object in the environment
+        """
+
         # initialize dimension of the world
         self.width = width
         self.height = height
 
         self.grid = np.zeros((self.height, self.width))
 
-        self.obstacles = []
-
         self.robot = robot
 
-    def render(self, mode='human', close=False):
+        self.obstacles = []
+        self.obstacles_coord = []
+        if obstacles is not None:
+            self.add_obstacles(obstacles)
+
+    def add_obstacles(self, obstacles):
+        self.obstacles.extend(obstacles)
+        for obst in obstacles:
+            for y in range(obst[2], obst[3]):
+                for x in range(obst[0], obst[1]):
+                    self.grid[y][x] = 1
+                    self.obstacles_coord.append([x, y])
+
+    def render(self):
         img = self.grid
         plt.clf()
         plt.imshow(img, cmap="binary", origin="upper")
@@ -24,4 +42,3 @@ class Environment:
         plt.gca().axes.get_yaxis().set_visible(False)
         plt.draw()
         plt.pause(0.001)
-
--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from src.Environment import Environment
 
-env = Environment()
+# environment without obstacles
+# env = Environment()
+# environment with obstacles
+env = Environment(100, 80, [[0, 40, 18, 21],
+                            [55, 58, 0, 50]])
 
+while KeyboardInterrupt:
+    env.render()
 env.render()
