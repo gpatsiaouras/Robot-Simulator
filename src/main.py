00142f7d4ff5e39bb5dfbe204a86b7b6063a27ac
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-02-17 01:49:09 +0100

    added walls in naive way, wip

--- src/main.py
+++ src/main.py
@@ -2,11 +2,6 @@ from Environment import Environment
 from robot import Robot
 import pygame
 
-pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Differential Robot Simulator')
-clock = pygame.time.Clock()
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 # environment without obstacles
@@ -16,10 +11,40 @@ env = Environment(100, 80, [[0, 60, 20, 23],
                             [80, 83, 0, 43],
                             [20, 83, 43, 46]])
 
+pygame.init()
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Differential Robot Simulator')
+clock = pygame.time.Clock()
+
+obstacles = [[0, 60, 20, 23],
+             [80, 83, 0, 43],
+             [20, 83, 43, 46]]
+
+obs_surfaces = []
+
+walls = []
+
+wall1 = pygame.Surface([60, 3])
+wall1.fill(BLACK)
+walls.append(wall1)
+
+wall2 = pygame.Surface([3, 40])
+wall2.fill(BLACK)
+walls.append(wall2)
+
+wall3 = pygame.Surface([63, 3])
+wall3.fill(BLACK)
+walls.append(wall3)
+
+
 robot = Robot(10, 0, [100, 100])
 
 robotImage = pygame.image.load('../assets/robot.png')
 robotImage = pygame.transform.scale(robotImage, (50, 50))
+
+gameDisplay.blit(wall1, (0, 20))
+gameDisplay.blit(wall2, (80, 0))
+gameDisplay.blit(wall3, (20, 43))
 
 gameDisplay.blit(robotImage, (100, 100))
 gameDisplay.fill(WHITE)
@@ -35,6 +60,10 @@ def game_loop():
         gameDisplay.fill(WHITE)
         robotImage = pygame.transform.rotate(robotImage, robot.rotation)
         # Redraw with the new position
+        gameDisplay.blit(wall1, (0, 20))
+        gameDisplay.blit(wall2, (80, 0))
+        gameDisplay.blit(wall3, (20, 43))
+
         gameDisplay.blit(robotImage, (robot.position[0], robot.position[1]))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,7 +83,6 @@ def game_loop():
                     robot.increment_both_wheels()
                 if event.key == pygame.K_g:
                     robot.decrement_both_wheels()
-
 
             # if event.type == pygame.KEYUP:
             #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
