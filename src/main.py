715bb92e5a645874d0e48c2a32a16d76ecea934b
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-02-17 13:47:20 +0100

    Kinematics work but:
    Movement is flipped, so increasing left wheel rotates the robot the other way around
    Rotation of the UI element its not working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from Environment import Environment
 from robot import Robot
 import pygame
+import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 # environment without obstacles
 # env = Environment()
 # environment with obstacles
@@ -12,7 +15,10 @@ env = Environment(100, 80, [[0, 60, 20, 23],
                             [20, 83, 43, 46]])
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Differential Robot Simulator')
 clock = pygame.time.Clock()
 
@@ -36,7 +42,6 @@ wall3 = pygame.Surface([63, 3])
 wall3.fill(BLACK)
 walls.append(wall3)
 
-
 robot = Robot(10, 0, [100, 100])
 
 robotImage = pygame.image.load('../assets/robot.png')
@@ -55,15 +60,31 @@ def game_loop():
     global robotImage
     while not gameExit:
         robot.move()
+        osd_text_1 = 'Robot Velocity:'
+        osd_text_2 = 'Left Wheel:{0}'.format(robot.left_wheel_velocity)
+        osd_text_3 = 'Right Wheel:{0}'.format(robot.right_wheel_velocity)
+        osd_text_4 = 'Theta:{0}'.format(robot.theta)
+
+        osd_1 = myfont.render(osd_text_1, False, (0, 0, 0))
+        osd_2 = myfont.render(osd_text_2, False, (0, 0, 0))
+        osd_3 = myfont.render(osd_text_3, False, (0, 0, 0))
+        osd_4 = myfont.render(osd_text_4, False, (0, 0, 0))
 
         # Draw everything white
         gameDisplay.fill(WHITE)
-        robotImage = pygame.transform.rotate(robotImage, robot.rotation)
         # Redraw with the new position
         gameDisplay.blit(wall1, (0, 20))
         gameDisplay.blit(wall2, (80, 0))
         gameDisplay.blit(wall3, (20, 43))
+        gameDisplay.blit(osd_1, (0, SCREEN_HEIGHT - 80))
+        gameDisplay.blit(osd_2, (0, SCREEN_HEIGHT - 60))
+        gameDisplay.blit(osd_3, (0, SCREEN_HEIGHT - 40))
+        gameDisplay.blit(osd_4, (0, SCREEN_HEIGHT - 20))
 
+        # Calculate degrees
+        print(robot.theta)
+        # TODO It doesn't work for now
+        # robotImage = pygame.transform.rotate(robotImage, robot.theta * 180 / np.pi)
         gameDisplay.blit(robotImage, (robot.position[0], robot.position[1]))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -83,13 +104,15 @@ def game_loop():
                     robot.increment_both_wheels()
                 if event.key == pygame.K_g:
                     robot.decrement_both_wheels()
+                if event.key == pygame.K_x:
+                    robot.stop_motors()
 
             # if event.type == pygame.KEYUP:
             #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             #         robot. = 0
 
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(25)
 
 
 game_loop()
--- src/robot.py
+++ src/robot.py
@@ -2,14 +2,16 @@ import numpy as np
 
 
 class Robot:
-    def __init__(self, diameter, initial_rotation, initial_position):
+    def __init__(self, diameter, initial_theta, initial_position):
         # Robot specifications
         self.diameter = diameter
         self.radius = diameter / 2
-        self.rotation = initial_rotation
         self.position = initial_position
 
-        # Velocity of wheel
+        # Rotation is in rads
+        self.theta = initial_theta
+
+        # Velocity of wheel in pixes/time
         self.MAX_SPEED = 10
         self.MIN_SPEED = -10
         self.left_wheel_velocity = 0
@@ -25,28 +27,29 @@ class Robot:
         pass
 
     def move(self):
-        # Calculate ω - angular velocity and change rotation of the robot
-        angular_velocity = (self.right_wheel_velocity - self.left_wheel_velocity) / self.diameter
-        self.rotation = self.rotation + angular_velocity
-
         if self.left_wheel_velocity != self.right_wheel_velocity:
+            # Calculate ω - angular velocity and change rotation of the robot
+            angular_velocity = (self.right_wheel_velocity - self.left_wheel_velocity) / self.diameter
+
+            # Keep theta from exploding
+            self.theta %= 2*np.pi
+
             R = (self.diameter / 2) * (self.left_wheel_velocity + self.right_wheel_velocity) / (
-                        self.right_wheel_velocity - self.left_wheel_velocity)
+                    self.right_wheel_velocity - self.left_wheel_velocity)
 
-            ICC = [self.position[0] - R * np.sin(self.rotation), self.position[1] + R * np.cos(self.rotation)]
+            ICCx = self.position[0] - R * np.sin(self.theta)
+            ICCy = self.position[1] + R * np.cos(self.theta)
 
-            matrixA = np.matrix([[np.cos(angular_velocity), -np.sin(angular_velocity), 0],[np.sin(angular_velocity), np.cos(angular_velocity), 0],[0, 0, 1]])
-            vectorA = np.array([self.position[0] - ICC[0], self.position[1] - ICC[1], self.rotation])
-            vectorB = np.array([ICC[0], ICC[1], self.rotation])
-            print(matrixA)
-            print(vectorA)
-            print(vectorB)
-            new_pos_rot = matrixA.dot(vectorA) + vectorB
+            matrix_a = np.matrix([[np.cos(angular_velocity), -np.sin(angular_velocity), 0], [np.sin(angular_velocity), np.cos(angular_velocity), 0], [0, 0, 1]])
+            vector_a = np.array([self.position[0] - ICCx, self.position[1] - ICCy, self.theta])
+            vector_b = np.array([ICCx, ICCy, angular_velocity])
+            new_pos_rot = matrix_a.dot(vector_a) + vector_b
+
             self.position = [new_pos_rot.item((0, 0)), new_pos_rot.item((0, 1))]
-            self.rotation = new_pos_rot.item((0, 2))
+            self.theta = new_pos_rot.item((0, 2))
         elif self.right_wheel_velocity != 0:
-            self.position[0] = self.position[0] + np.cos(self.right_wheel_velocity)
-            self.position[1] = self.position[1] + np.sin(self.right_wheel_velocity)
+            self.position[0] = self.position[0] + (self.right_wheel_velocity * np.cos(self.theta))
+            self.position[1] = self.position[1] + (self.right_wheel_velocity * np.sin(self.theta))
 
     def increment_left_wheel(self):
         if self.left_wheel_velocity + 1 <= self.MAX_SPEED and self.left_wheel_velocity >= self.MIN_SPEED:
@@ -71,3 +74,7 @@ class Robot:
     def decrement_both_wheels(self):
         self.decrement_left_wheel()
         self.decrement_right_wheel()
+
+    def stop_motors(self):
+        self.right_wheel_velocity = 0
+        self.left_wheel_velocity = 0
