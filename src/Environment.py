0d523bdc3cf33722497e7c68cb0db94e1bc321a7
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-02-17 18:43:09 +0100

    fixed osd not working

--- src/Environment.py
+++ src/Environment.py
@@ -34,23 +34,13 @@ class Environment:
 
         pygame.init()
         pygame.font.init()
-        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
         self.gameDisplay = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Differential Robot Simulator')
         self.clock = pygame.time.Clock()
 
         self.add_obstacles()
-
-        osd_text_1 = 'Robot Velocity:'
-        osd_text_2 = 'Left Wheel:{0}'.format(robot.left_wheel_velocity)
-        osd_text_3 = 'Right Wheel:{0}'.format(robot.right_wheel_velocity)
-        osd_text_4 = 'Theta:{0}'.format(robot.theta)
-
-        self.osd_1 = myfont.render(osd_text_1, False, (0, 0, 0))
-        self.osd_2 = myfont.render(osd_text_2, False, (0, 0, 0))
-        self.osd_3 = myfont.render(osd_text_3, False, (0, 0, 0))
-        self.osd_4 = myfont.render(osd_text_4, False, (0, 0, 0))
 
     def add_obstacles(self):
         self.wall_lenght = (self.height / 4) * 3 if self.height < self.width else (self.width / 4) * 3
@@ -78,12 +68,21 @@ class Environment:
         self.gameDisplay.blit(self.wall1, (0, self.height/ 3))
         self.gameDisplay.blit(self.wall2, (self.width / 4 * 3, 0))
         self.gameDisplay.blit(self.wall3, (self.width / 4, (self.height / 4) * 3))
-        self.gameDisplay.blit(self.osd_1, (0, self.height - 80))
-        self.gameDisplay.blit(self.osd_2, (0, self.height - 60))
-        self.gameDisplay.blit(self.osd_3, (0, self.height - 40))
-        self.gameDisplay.blit(self.osd_4, (0, self.height - 20))
 
         if self.robot is not None:
+            osd_text_1 = 'Robot Velocity:'
+            osd_text_2 = 'Left Wheel:{0}'.format(self.robot.left_wheel_velocity)
+            osd_text_3 = 'Right Wheel:{0}'.format(self.robot.right_wheel_velocity)
+            osd_text_4 = 'Theta:{0}'.format(self.robot.theta)
+            self.osd_1 = self.myfont.render(osd_text_1, False, (0, 0, 0))
+            self.osd_2 = self.myfont.render(osd_text_2, False, (0, 0, 0))
+            self.osd_3 = self.myfont.render(osd_text_3, False, (0, 0, 0))
+            self.osd_4 = self.myfont.render(osd_text_4, False, (0, 0, 0))
+            self.gameDisplay.blit(self.osd_1, (0, self.height - 80))
+            self.gameDisplay.blit(self.osd_2, (0, self.height - 60))
+            self.gameDisplay.blit(self.osd_3, (0, self.height - 40))
+            self.gameDisplay.blit(self.osd_4, (0, self.height - 20))
+
             pygame.draw.circle(self.gameDisplay, self.BLACK, [int(self.robot.position[0]), int(self.robot.position[1])], 50, 3)
             end_x = self.robot.diameter * np.cos(2 * np.pi + self.robot.theta)
             end_y = self.robot.diameter * np.sin(2 * np.pi + self.robot.theta)
@@ -91,3 +90,4 @@ class Environment:
                              [int(self.robot.position[0]), int(self.robot.position[1])],
                              [int(self.robot.position[0] + end_x), int(self.robot.position[1] + end_y)],
                              3)
+
