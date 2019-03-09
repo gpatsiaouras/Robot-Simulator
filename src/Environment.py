970aaccda5d88fb07603d4080caae5b4be7929d1
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-03-09 19:23:13 +0100

    Added Dust to the environment and refactored a bit

--- src/Environment.py
+++ src/Environment.py
@@ -6,7 +6,12 @@ class Environment:
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     GREEN = (0, 255, 0)
+    BROWN = (222, 184, 135)
+    LIGHTYELLOW = (250, 235, 215)
+    LIGHTGREEN = (144, 238, 144)
     ROBOT_DIAMETER = 50
+    DUST_DOTS_COLOR = LIGHTGREEN
+    DUST_DOT_WIDTH = 3
     PADDING = 50
 
     def __init__(self, width=800, height=800, obstacles=None, robot=None):
@@ -21,13 +26,6 @@ class Environment:
         self.width = width
         self.height = height
 
-        # initialize matrix with zeros representing the map
-        self.grid = np.zeros((self.height, self.width))
-        # self.map = pygame.Surface([self.height + self.PADDING, self.width + self.PADDING])
-        self.map = pygame.Surface([self.height, self.width])
-        self.map.fill((255, 255, 255))
-        self.rect = self.map.get_rect()
-
         pygame.init()
         pygame.font.init()
         self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -38,6 +36,12 @@ class Environment:
 
         # initialize robot
         self.robot = robot
+
+        # Initialize dust
+        self.dust_dots_intensity = 4
+        self.dust_matrix_width = int(self.width / self.dust_dots_intensity)
+        self.dust_matrix_height = int(self.height / self.dust_dots_intensity)
+        self.dust_matrix, self.dust_point = self.initialize_dust()
 
         # OBSTACLES
 
@@ -73,10 +77,10 @@ class Environment:
 
             # slope m
             if self.obstacles_coord[count][3] - self.obstacles_coord[count][1] == 0:
-                    self.obstacles_parameters[count][0] = 0
+                self.obstacles_parameters[count][0] = 0
             else:
                 if self.obstacles_coord[count][2] - self.obstacles_coord[count][0] == 0:
-                        self.obstacles_parameters[count][0] = float('inf')
+                    self.obstacles_parameters[count][0] = float('inf')
                 else:
                     self.obstacles_parameters[count][0] = (self.obstacles_coord[count][3] - self.obstacles_coord[count][
                         1]) / ((self.obstacles_coord[count][2] -
@@ -96,30 +100,39 @@ class Environment:
             print(self.obstacles_coord)
             print(self.obstacles_parameters)
 
-    def redraw_obstacles(self):
+    def initialize_dust(self):
+        # Zero(0) means that there is dust.
+        # Minus one (-1) means that robot passed by this spot so there is not dust anymore
+        dust_matrix = np.ones((self.dust_matrix_width, self.dust_matrix_height))
+        dust_point = [[None for j in range(self.dust_matrix_width)] for i in range(self.dust_matrix_height)]
+
+        return dust_matrix, dust_point
+
+    def reset_background(self):
+        # Draw everything white
+        self.gameDisplay.fill(self.WHITE)
+
+    def draw_dust(self):
+        for i in range(self.dust_matrix_width):
+            for j in range(self.dust_matrix_height):
+                # First check if the dust_point is not None because it could be the first time that we draw
+                # Then if yes, remove the footprint of the robot from the dust matrix by changing the value to 0
+                if self.dust_point[i][j] is not None and self.robot.robot_rect.colliderect(self.dust_point[i][j]):
+                    self.dust_matrix[i][j] = 0
+
+                # Draw this dot if the dust_matrix has the value 1
+                if self.dust_matrix[i][j] == 1:
+                    self.dust_point[i][j] = pygame.draw.circle(self.gameDisplay, self.DUST_DOTS_COLOR,
+                                                         [i * self.dust_dots_intensity,
+                                                          j * self.dust_dots_intensity], self.DUST_DOT_WIDTH)
+
+    def draw_obstacles(self):
         for count, obst in enumerate(self.obstacles_coord):
             pygame.draw.line(self.gameDisplay, self.BLACK,
                              (self.obstacles_coord[count][0], self.obstacles_coord[count][1]),
                              (self.obstacles_coord[count][2], self.obstacles_coord[count][3]), 3)
 
-        # pygame.draw.line(self.gameDisplay, self.BLACK, (self.obstacles_coord[0, 0], self.obstacles_coord[0, 1]),
-        #                  (self.obstacles_coord[0, 2], self.obstacles_coord[0, 3]), 3)
-        # pygame.draw.line(self.gameDisplay, self.BLACK, (self.obstacles_coord[1, 0], self.obstacles_coord[1, 1]),
-        #                  (self.obstacles_coord[1, 2], self.obstacles_coord[1, 3]), 3)
-        # pygame.draw.line(self.gameDisplay, self.BLACK, (self.obstacles_coord[2, 0], self.obstacles_coord[2, 1]),
-        #                  (self.obstacles_coord[2, 2], self.obstacles_coord[2, 3]), 3)
-
-    def render(self):
-
-        # Draw everything white
-        self.gameDisplay.fill(self.WHITE)
-        # Redraw with the new position
-        self.redraw_obstacles()
-
-        # Check validity of rendering
-        if self.robot is None:
-            return
-
+    def draw_osd(self):
         osd_text_1 = 'Robot Velocity:'
         osd_text_2 = 'Left Wheel:{0}'.format(self.robot.left_wheel_velocity)
         osd_text_3 = 'Right Wheel:{0}'.format(self.robot.right_wheel_velocity)
@@ -128,24 +141,32 @@ class Environment:
         osd_2 = self.myfont.render(osd_text_2, False, (0, 0, 0))
         osd_3 = self.myfont.render(osd_text_3, False, (0, 0, 0))
         osd_4 = self.myfont.render(osd_text_4, False, (0, 0, 0))
-        self.gameDisplay.blit(osd_1, (0, self.height - 80))
-        self.gameDisplay.blit(osd_2, (0, self.height - 60))
-        self.gameDisplay.blit(osd_3, (0, self.height - 40))
-        self.gameDisplay.blit(osd_4, (0, self.height - 20))
+        self.gameDisplay.blit(osd_1, (20, self.height - 100))
+        self.gameDisplay.blit(osd_2, (20, self.height - 80))
+        self.gameDisplay.blit(osd_3, (20, self.height - 60))
+        self.gameDisplay.blit(osd_4, (20, self.height - 40))
 
+    def draw_robot(self):
+        # Draw the circle of the robot
         self.robot.robot_rect = pygame.draw.circle(self.gameDisplay, self.BLACK,
                                                    [int(self.robot.position[0]), int(self.robot.position[1])],
                                                    self.robot.radius, 3)
+
+        # Draw the small line indicating the direction that the robot is moving
+        # End_x and end_y represent where the small line should end, starting from the centre of the circle
         end_x = self.robot.radius * np.cos(2 * np.pi + self.robot.theta)
         end_y = self.robot.radius * np.sin(2 * np.pi + self.robot.theta)
+
+        # Draw the line
         pygame.draw.line(self.gameDisplay, self.BLACK,
                          [int(self.robot.position[0]), int(self.robot.position[1])],
                          [int(self.robot.position[0] + end_x), int(self.robot.position[1] + end_y)],
                          3)
 
+    def draw_sensors(self):
         for sensor in range(len(self.robot.sensors_values)):
             osd_sensor_text = str(round(self.robot.sensors_values[sensor]))
-            osd_sensor = self.myfont.render(osd_sensor_text, False, (0, 255, 0))
+            osd_sensor = self.myfont.render(osd_sensor_text, False, self.BLACK)
             self.gameDisplay.blit(osd_sensor,
                                   (self.robot.sensors_coords[sensor, 2], self.robot.sensors_coords[sensor, 3]))
 
@@ -153,8 +174,18 @@ class Environment:
         if len(self.robot.sensors_coords) != 0:
             updated_sensors = []
             for sensor in self.robot.sensors_coords:
-                updated_sensors.append(pygame.draw.line(self.gameDisplay, self.GREEN,
+                updated_sensors.append(pygame.draw.line(self.gameDisplay, self.BLACK,
                                                         [int(sensor[0]), int(sensor[1])],
                                                         [int(sensor[2]), int(sensor[3])],
                                                         3))
             self.robot.sensors_rects = updated_sensors
+
+    def render(self):
+        self.reset_background()
+
+        # Draw components. Ordering of draw matters for the window to look nice
+        self.draw_robot()
+        self.draw_dust()
+        self.draw_osd()
+        self.draw_obstacles()
+        self.draw_sensors()
