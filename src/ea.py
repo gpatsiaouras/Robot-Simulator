339d889821158a2fc4f3ad36e9c23380bb1de6b3
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-03-11 10:49:16 +0100

    Added the fitness formula

--- src/ea.py
+++ src/ea.py
@@ -2,6 +2,12 @@ import numpy as np
 import rooms
 from main import Simulator
 
+# Fitness Formula Parameters
+COLLISION_PENALTY = -0.5
+COLLISION_WEIGHT = 0.7
+DUST_MULTIPLIER = 100
+DUST_WEIGHT = 0.3
+
 
 class EvolutionaryAlgorithm:
     def __init__(self, number_of_generations, robots_per_generation, exploration_steps):
@@ -23,7 +29,16 @@ class EvolutionaryAlgorithm:
             self.simulators_list.append(sim)
 
     def fitness(self):
-        pass
+        fitness = []
+        for sim_num in range(self.robots_per_generation):
+            dust_coverage = self.simulators_list[sim_num].env.dust_coverage
+            collisions = self.simulators_list[sim_num].robot.collisions
+
+            fitness_formula = (DUST_WEIGHT * dust_coverage * DUST_MULTIPLIER
+                               + COLLISION_WEIGHT * collisions * COLLISION_PENALTY) / (DUST_WEIGHT + COLLISION_WEIGHT)
+            fitness.append(fitness_formula)
+
+        return fitness
 
     def select_mating_pool(self):
         pass
@@ -38,12 +53,15 @@ class EvolutionaryAlgorithm:
         # new_population = np.random.uniform(low=0.0, high=1.0, size=self.population_size)
 
         for generation in range(self.number_of_generations):
+            print("Generation {0}#".format(generation))
             # For each chromesome (robot in a different environment) run the simulation
             for sim_num in range(self.robots_per_generation):
                 self.simulators_list[sim_num].run()
 
             # Take the fitness of each chromosome in the population
-            # fitness = fitness(self.simulators_list[sim_num])
+            print("Fitness:", end="")
+            fitness = self.fitness()
+            print(fitness, end="\n\n")
             # Select the best parents in the population
             # parents = self.select_mating_pool(new_population, fitness, number_of_parents_mating)
 
