3b499eb9f8fef551ffaa9d8652cd92249d47c241
Alessandro Scoppio <alessandroscoppio@gmail.com>
2019-03-11 03:03:55 +0100

    neural network now recurrent, check comments for explanations

--- src/ann.py
+++ src/ann.py
@@ -6,16 +6,22 @@ def sigmoid(x):
 
 
 class ANN:
-    def __init__(self, input_vector, weights1, weights2):
-        self.input_vector = input_vector
+    def __init__(self, input_neurons=12, weights1=None, weights2=None, hidden_neurons=5):
+        self.input_vector = input_neurons
+        # initialize previous activations to zero, so that at first initialization of the network it just concatenetes zeros
+        self.prev_activation = np.zeros((hidden_neurons, 1))
         self.weights1 = weights1
         self.weights2 = weights2
         self.output_vector = np.random.rand(2, 1)
 
     def feed_forward(self):
-        middle_layer = sigmoid(np.dot(self.input_vector.T, self.weights1))
+        # in first step take as input sensors values + (that stands for concatenates being vectors) + activations of previous iteration
+        middle_layer = sigmoid(np.dot(self.input_vector.T + self.prev_activation.T, self.weights1))
+        # after computing new activations store them for next iterations
+        self.prev_activation = middle_layer
         self.output_vector = sigmoid(np.dot(middle_layer, self.weights2))
 
+    # TODO: This get_ouput could easily be a return in the feedforward method
     def get_output(self):
         return self.output_vector
 
@@ -29,7 +35,7 @@ if __name__ == '__main__':
     weights2 = np.random.rand(5, 2)
 
     # Initiate Neural Network
-    ann = ANN(input_vector=input_vector, weights1=weights1, weights2=weights2)
+    ann = ANN(input_neurons=input_vector, weights1=weights1, weights2=weights2)
     ann.feed_forward()
 
     print(ann.get_output())
